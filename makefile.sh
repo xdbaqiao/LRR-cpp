2ea0cfd219cde779002d0b12a30c0d5ef427372c
lvzongting <lvzongting@gmail.com>
2013-04-11 14:50:56 +0800

    Update makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -1,7 +1,7 @@
  g++ `pkg-config --cflags --libs eigen3` -o lrr LowRankRepresentation.cpp
 
 #gcc with mex
-#g++ -fPIC -DMATLAB_MEX_FILE `pkg-config --cflags --libs eigen3` -I/home/lvzongting/opt/matlab/extern/include -c lrralm.c
+#g++ -fPIC -DMATLAB_MEX_FILE `pkg-config --cflags --libs eigen3` -I/home/lvzongting/opt/matlab/extern/include -c alm_lrr_l21.c
 
 #mex with gcc
-#mex -I/usr/include/eigen3 lrralm.cpp
+#mex -I/usr/include/eigen3 alm_lrr_l21.cpp
